ffe10b45890862621c30594de78341ab836f3bc8
Joshua Scheeel <jscheel42@gmail.com>
2014-11-01 19:30:19 -0700

    First commit stub

---
+++ src/main/java/com/sandvichvonnom/parsem3/App.java
@@ -0,0 +1,13 @@
+package com.sandvichvonnom.parsem3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
