ba03c972aa87372b867178797638917225699967
Joshua Scheel <jscheel@armus.com>
2014-12-05 17:08:44 -0800

    Fixes to rename

--- src/test/java/com/sandvichvonnom/parsem3/AppTestmain.java
+++ src/test/java/com/sandvichvonnom/parsem3/AppTestmain.java
@@ -15,7 +15,7 @@ public class AppTestmain
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public AppTestmain( String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class AppTestmain
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( AppTestmain.class );
     }
 
     /**
