65eb5828d25bf787887aac0dd3cbf67e49432cb2
Joshua Scheel <jscheel42@gmail.com>
2015-07-16 20:15:12 -0700

    Use TestClass1 in backend

--- backend/src/main/java/com/sandvichvonnom/parsem3/AppBackend.java
+++ backend/src/main/java/com/sandvichvonnom/parsem3/AppBackend.java
@@ -4,7 +4,17 @@ package com.sandvichvonnom.parsem3;
  * Created by jscheel on 7/16/15.
  */
 public class AppBackend {
+
     public static void main( String[] args ) {
-        System.out.println("Hello, I am core!");
+
+        System.out.println("Hello, I am backend!");
+
+        TestClass1 test1 = new TestClass1();
+
+        test1.setName("AppBackend");
+
+        test1.sayName();
+
     }
+
 }
